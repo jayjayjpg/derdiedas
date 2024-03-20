9742221d0f1e04fa4f5c8f2103e92074b560212d
Jessy Jordan <jayjayjpg@gmail.com>
2024-03-20 12:01:14 +0000

    feat(exercise): add coming soon section

--- app/controllers/exercises/index.js
+++ app/controllers/exercises/index.js
@@ -1,6 +1,9 @@
 import Controller from '@ember/controller';
 import { service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class ExercisesIndexController extends Controller {
   @service practiceSession;
+
+  @action noop() {}
 }
