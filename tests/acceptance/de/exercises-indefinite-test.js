ecea9d2859994eb6a6cde2589d5f6822cd339a5a
Jessy Jordan <jayjayjpg@gmail.com>
2024-05-25 22:30:54 +0200

    test(exerc): add test coverage for indefinite art. exercises

--- tests/acceptance/de/exercises-indefinite-test.js
+++ tests/acceptance/de/exercises-indefinite-test.js
@@ -3,29 +3,19 @@ import { click, fillIn, visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'derdiedas/tests/helpers';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 
-module('Acceptance | de/exercises/definite', function (hooks) {
+module('Acceptance | de/exercises/indefinite', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
   hooks.beforeEach(function () {
-    this.server.createList('question', 3);
+    this.server.createList('indefinite-question', 3);
   });
 
-  test('visiting /exercises', async function (assert) {
+  test('visiting /exercises/indefinite', async function (assert) {
     await visit('/exercises');
+    await click('[data-test-exercise-choose-indefinite]');
 
-    assert.strictEqual(currentURL(), '/exercises');
-    assert.dom('[data-test-exercise-go-back-home]').exists();
-    assert.dom('[data-test-exercise-primer]').exists();
-    assert.dom('[data-test-exercise-choose-definite]').exists();
-    assert.dom('[data-test-exercise-choose-indefinite]').exists();
-  });
-
-  test('visiting /exercises/definite', async function (assert) {
-    await visit('/exercises');
-    await click('[data-test-exercise-choose-definite]');
-
-    assert.strictEqual(currentURL(), '/exercises/definite');
+    assert.strictEqual(currentURL(), '/exercises/indefinite');
     assert.dom('[data-test-exercise-go-back-home]').exists();
     assert.dom('[data-test-exercise-primer]').exists();
     assert.dom('[data-test-exercise-start]').exists();
@@ -36,9 +26,9 @@ module('Acceptance | de/exercises/definite', function (hooks) {
 
     assert.strictEqual(currentURL(), '/exercises');
 
-    await click('[data-test-exercise-choose-definite]');
+    await click('[data-test-exercise-choose-indefinite]');
 
-    assert.strictEqual(currentURL(), '/exercises/definite');
+    assert.strictEqual(currentURL(), '/exercises/indefinite');
 
     await click('[data-test-exercise-start]');
 
@@ -51,7 +41,7 @@ module('Acceptance | de/exercises/definite', function (hooks) {
 
     assert.strictEqual(currentURL(), '/exercises');
 
-    await click('[data-test-exercise-choose-definite]');
+    await click('[data-test-exercise-choose-indefinite]');
     await click('[data-test-exercise-start]');
 
     assert.strictEqual(currentURL(), '/exercises/1');
@@ -64,7 +54,7 @@ module('Acceptance | de/exercises/definite', function (hooks) {
 
   test('a user can proceed to the next question once the form is submitted', async function (assert) {
     await visit('/exercises');
-    await click('[data-test-exercise-choose-definite]');
+    await click('[data-test-exercise-choose-indefinite]');
     await click('[data-test-exercise-start]');
 
     assert.strictEqual(currentURL(), '/exercises/1');
@@ -78,24 +68,24 @@ module('Acceptance | de/exercises/definite', function (hooks) {
 
   test('a user sees a success state if an answer is answered correctly', async function (assert) {
     await visit('/exercises');
-    await click('[data-test-exercise-choose-definite]');
+    await click('[data-test-exercise-choose-indefinite]');
     await click('[data-test-exercise-start]');
 
     assert.strictEqual(currentURL(), '/exercises/1');
 
-    await fillIn('[data-test-question-form-input]', 'das');
+    await fillIn('[data-test-question-form-input]', 'ein');
     await click('[data-test-question-form-submit]');
 
     assert
       .dom('[data-test-question-form-correct-answer]')
-      .includesText('Ich gehe in das Haus');
+      .includesText('Ich gehe in ein Haus');
     assert.dom('[data-test-question-form-success]').exists();
     assert.dom('[data-test-question-form-difference]').doesNotExist();
   });
 
   test('a user sees an error explanation if an answer is answered incorrectly', async function (assert) {
     await visit('/exercises');
-    await click('[data-test-exercise-choose-definite]');
+    await click('[data-test-exercise-choose-indefinite]');
     await click('[data-test-exercise-start]');
 
     assert.strictEqual(currentURL(), '/exercises/1');
@@ -105,7 +95,7 @@ module('Acceptance | de/exercises/definite', function (hooks) {
 
     assert
       .dom('[data-test-question-form-correct-answer]')
-      .includesText('Ich gehe in das Haus');
+      .includesText('Ich gehe in ein Haus');
     assert.dom('[data-test-question-form-success]').doesNotExist();
     assert.dom('[data-test-question-form-difference]').exists();
     assert
@@ -113,17 +103,17 @@ module('Acceptance | de/exercises/definite', function (hooks) {
       .includesText('foo');
     assert
       .dom('[data-test-question-form-difference-expected]')
-      .includesText('das');
+      .includesText('ein');
   });
 
   test('a user can finish a practice session', async function (assert) {
     await visit('/exercises');
-    await click('[data-test-exercise-choose-definite]');
+    await click('[data-test-exercise-choose-indefinite]');
     await click('[data-test-exercise-start]');
 
     assert.strictEqual(currentURL(), '/exercises/1');
 
-    await fillIn('[data-test-question-form-input]', 'der');
+    await fillIn('[data-test-question-form-input]', 'ein');
     await click('[data-test-question-form-submit]');
     await click('[data-test-exercise-next]');
 
@@ -148,7 +138,7 @@ module('Acceptance | de/exercises/definite', function (hooks) {
 
   test('exiting the session resets its state', async function (assert) {
     await visit('/exercises');
-    await click('[data-test-exercise-choose-definite]');
+    await click('[data-test-exercise-choose-indefinite]');
     await click('[data-test-exercise-start]');
 
     assert.strictEqual(currentURL(), '/exercises/1');
@@ -164,7 +154,7 @@ module('Acceptance | de/exercises/definite', function (hooks) {
     assert.strictEqual(currentURL(), '/');
 
     await click('[data-test-navigation-exercise-de]');
-    await click('[data-test-exercise-choose-definite]');
+    await click('[data-test-exercise-choose-indefinite]');
     await click('[data-test-exercise-start]');
 
     assert.strictEqual(currentURL(), '/exercises/1');
